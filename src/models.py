752d87e6db35d0e1362071fadbcd19f4b73843b7
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-07-21 04:18:22 +0300

    updated models.py (added db models)

---
+++ src/models.py
@@ -0,0 +1,34 @@
+import decimal
+from uuid import uuid4
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import VARCHAR, TEXT, DECIMAL, ForeignKey, UUID
+
+
+from src.database import Base
+
+
+class Menus(Base):
+    __tablename__ = "menus"
+
+    menu_id: Mapped[uuid4] = mapped_column(UUID, primary_key=True, default=uuid4)
+    title: Mapped[str] = mapped_column(VARCHAR(80), nullable=True)
+    description: Mapped[str] = mapped_column(TEXT)
+
+
+class SubMenus(Base):
+    __tablename__ = "submenus"
+
+    submenu_id: Mapped[uuid4] = mapped_column(UUID, primary_key=True, default=uuid4)
+    title: Mapped[str] = mapped_column(VARCHAR(80), nullable=True)
+    description: Mapped[str] = mapped_column(TEXT)
+    menu_id: Mapped[uuid4] = mapped_column(ForeignKey("menus.menu_id", ondelete="CASCADE"))
+
+
+class Dishes(Base):
+    __tablename__ = "dishes"
+
+    dishes_id: Mapped[uuid4] = mapped_column(UUID, primary_key=True, default=uuid4)
+    title: Mapped[str] = mapped_column(VARCHAR(80), nullable=True)
+    price: Mapped[decimal.Decimal] = mapped_column(DECIMAL(2, 10))
+    submenu_id: Mapped[uuid4] = mapped_column(ForeignKey("submenus.submenu_id", ondelete="CASCADE"))
