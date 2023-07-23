eb04f7e26588a2c20885fcc4cdf79799f421b816
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-07-23 15:46:29 +0300

    updated src/service.py (deleted unused imports; created funcs get_submenus and get_submenu_by_id; renamed id in func create_response)

--- src/service.py
+++ src/service.py
@@ -1,12 +1,3 @@
-import uuid
-
-from fastapi import HTTPException
-from sqlalchemy import CursorResult
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.database import get_async_session
-
-
 class ConvertDataToJson:
 
     @staticmethod
@@ -25,6 +16,27 @@ class ConvertDataToJson:
                         "title": title,
                         "description": desc,
                         "submenus_count": submenus_count,
+                        "dishes_count": dishes_count
+                    }
+                ]
+            )
+        return json_data
+
+    @staticmethod
+    async def get_submenus(result):
+        json_data = []
+
+        for row in result.fetchall():
+            if not row:
+                return json_data
+            menu_id, title, desc, dishes_count = row[0], row[1], row[2], row[3]
+
+            json_data.extend(
+                [
+                    {
+                        "id": menu_id,
+                        "title": title,
+                        "description": desc,
                         "dishes_count": dishes_count
                     }
                 ]
@@ -53,12 +65,28 @@ class ConvertDataToJson:
         row = result.fetchone()
         if not row:
             return None
-        menu_id, title, desc = row[0], row[1], row[2]
+        current_id, title, desc = row[0], row[1], row[2]
 
         json_data = {
-            "id": menu_id,
+            "id": current_id,
             "title": title,
             "description": desc
+        }
+
+        return json_data
+
+    @staticmethod
+    async def get_submenu_by_id(result):
+        row = result.fetchone()
+        if not row:
+            return None
+        submenu_id, title, desc, dishes_count = row[0], row[1], row[2], row[3]
+
+        json_data = {
+            "id": submenu_id,
+            "title": title,
+            "description": desc,
+            "dishes_count": dishes_count
         }
 
         return json_data
