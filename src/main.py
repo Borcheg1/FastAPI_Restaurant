86a8767b0ed23f4b85af92f1bdcc84ec1fc1aba1
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-07-23 11:26:26 +0300

    updated src/main.py (created func init_db)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from src.database import create_tables
 from src.menus.router import router as menus_router
 
 
 app = FastAPI(title="Restaurant API")
 
 app.include_router(menus_router)
+
+
+@app.on_event("startup")
+async def init_db() -> None:
+    await create_tables()
