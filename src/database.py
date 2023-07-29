61c0039acc8b9c8d9f5dbfb101c1dc67d3a1dcbe
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-07-29 08:54:43 +0300

    reformated code

--- src/database.py
+++ src/database.py
@@ -1,6 +1,8 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncSession, async_sessionmaker, create_async_engine
+)
 from sqlalchemy.orm import DeclarativeBase
 
 from src.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
--- src/models.py
+++ src/models.py
@@ -25,7 +25,8 @@ class Submenus(Base):
     id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=get_uuid)
     title: Mapped[str] = mapped_column(VARCHAR(80), nullable=True, unique=True)
     description: Mapped[str] = mapped_column(TEXT)
-    menu_id: Mapped[UUID] = mapped_column(ForeignKey("menus.id", ondelete="CASCADE"))
+    menu_id: Mapped[UUID] = mapped_column(
+        ForeignKey("menus.id", ondelete="CASCADE"))
 
 
 class Dishes(Base):
@@ -35,4 +36,5 @@ class Dishes(Base):
     title: Mapped[str] = mapped_column(VARCHAR(80), nullable=True, unique=True)
     description: Mapped[str] = mapped_column(TEXT)
     price: Mapped[decimal.Decimal] = mapped_column(DECIMAL(10, 2))
-    submenu_id: Mapped[UUID] = mapped_column(ForeignKey("submenus.id", ondelete="CASCADE"))
+    submenu_id: Mapped[UUID] = mapped_column(
+        ForeignKey("submenus.id", ondelete="CASCADE"))
