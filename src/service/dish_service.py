2f8bb8505ff92faa2d3eb7935076a4cfb419ea76
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-08-04 18:43:45 +0300

    created service layer

--- src/service/dish_service.py
+++ src/service/dish_service.py
@@ -1,185 +1,48 @@
 from uuid import UUID
 
-from fastapi import HTTPException
-from sqlalchemy import delete, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.models import Dishes, Submenus
+from src.repository.dish_repository import DishRepository
 from src.schemas import RequestDish, ResponseDish, ResponseMessage
 
 
 class DishService:
-    main_query = select(
-        Dishes.id, Dishes.title, Dishes.description, Dishes.price
-    ).group_by(Dishes.id)
-
-    col = Dishes.__table__.columns.keys()
-
-    @staticmethod
-    async def check_exists_id_and_title(
-        session: AsyncSession,
-        target_menu_id: UUID,
-        target_submenu_id: UUID,
-        target_dish_id: UUID | None = None,
-        title: str | None = None,
-    ) -> None:
-        query_check = (
-            select(Dishes.title)
-            .outerjoin(Submenus, Submenus.menu_id == target_menu_id)
-            .where(Dishes.submenu_id == target_submenu_id, Dishes.id == target_dish_id)
-        )
-        query_title = (
-            select(Submenus.id, Dishes.title)
-            .outerjoin(Dishes, Dishes.title == title)
-            .where(Submenus.menu_id == target_menu_id, Submenus.id == target_submenu_id)
-        )
-        if target_dish_id:
-            result = await session.execute(query_check)
-            row = result.first()
-
-            if not row:
-                raise HTTPException(status_code=404, detail='dish not found')
-
-            if title:
-                if row[0] == title:
-                    raise HTTPException(
-                        status_code=409, detail='This title already exists'
-                    )
-
-        if title and not target_dish_id:
-            result = await session.execute(query_title)
-            row = result.first()
-            if not row:
-                raise HTTPException(status_code=404, detail='menu not found')
-            if row[1]:
-                raise HTTPException(status_code=409, detail='This title already exists')
+    def __init__(self):
+        self.dish_repository = DishRepository()
 
     async def get_all_dishes(
-        self, target_menu_id: UUID, target_submenu_id: UUID, session: AsyncSession
-    ) -> list[ResponseDish]:
-        """Get dishes list from db and return them."""
-        query_check_menu = select(Submenus.id).where(Submenus.menu_id == target_menu_id)
-        result_check = await session.execute(query_check_menu)
-        if not result_check.first():
-            return []
-        result = await session.execute(
-            self.main_query.where(Dishes.submenu_id == target_submenu_id)
-        )
-        rows = result.fetchall()
-        dishes = []
-        for row in rows:
-            dishes_dict = dict(zip(self.col, row))
-            dishes_dict['price'] = str(dishes_dict['price'])
-            dishes.append(ResponseDish(**dishes_dict))
-        return dishes
+        self, session: AsyncSession, menu_id: UUID, submenu_id: UUID
+    ) -> list[ResponseDish] | list:
+        return await self.dish_repository.get_all(session, menu_id, submenu_id)
 
     async def get_dish_by_id(
-        self,
-        target_menu_id: UUID,
-        target_submenu_id: UUID,
-        target_dish_id: UUID,
-        session: AsyncSession,
+        self, session: AsyncSession, menu_id: UUID, submenu_id: UUID,
+        dish_id: UUID
     ) -> ResponseDish:
-        """Get dish from db and return it.
-
-        target_dish_id: Current dish id.
-        """
-        query_check_menu = select(Submenus.id).where(
-            Submenus.menu_id == target_menu_id, Submenus.id == target_submenu_id
+        return await self.dish_repository.get_by_id(
+            session, menu_id, submenu_id, dish_id
         )
-        result_check = await session.execute(query_check_menu)
-
-        if not result_check.first():
-            raise HTTPException(status_code=404, detail='menu or submenu not found')
-
-        query = self.main_query.where(target_dish_id == Dishes.id)
-        result = await session.execute(query)
-        row = result.fetchone()
-
-        if not row:
-            raise HTTPException(status_code=404, detail='dish not found')
-
-        dish = dict(zip(self.col, row), **{'price': str(row[-1])})
-        return ResponseDish(**dish)
 
     async def add_dish(
-        self,
-        target_menu_id: UUID,
-        target_submenu_id: UUID,
-        new_dish: RequestDish,
-        session: AsyncSession,
+        self, session: AsyncSession, menu_id: UUID, submenu_id: UUID,
+        new_menu: RequestDish
     ) -> ResponseDish:
-        """Add new dish to db and return it.
-
-        target_submenu_id: Current submenu id.
-        new_dish: Pydantic schema for request body.
-        """
-        new_dish_dict = dict(new_dish, **{'submenu_id': target_submenu_id})
-        stmt = insert(Dishes).values(**new_dish_dict)
-        query = self.main_query.where(Dishes.title == new_dish.title)
-
-        await self.check_exists_id_and_title(
-            session, target_menu_id, target_submenu_id, title=new_dish.title
+        return await self.dish_repository.add(
+            session, menu_id, submenu_id, new_menu
         )
-
-        await session.execute(stmt)
-        await session.commit()
-        result = await session.execute(query)
-        row = result.fetchone()
-        dish = dict(zip(self.col, row), **{'price': str(row[-1])})
-        return ResponseDish(**dish)
 
     async def update_dish(
-        self,
-        target_menu_id: UUID,
-        target_submenu_id: UUID,
-        target_dish_id: UUID,
-        new_dish: RequestDish,
-        session: AsyncSession,
+        self, session: AsyncSession, menu_id: UUID, submenu_id: UUID,
+        dish_id: UUID, new_menu: RequestDish
     ) -> ResponseDish:
-        """Update dish in db and return it.
-
-        target_dish_id: Current dish id.
-        new_dish: Pydantic schema for request body.
-        """
-        stmt = (
-            update(Dishes).where(target_dish_id == Dishes.id).values(**dict(new_dish))
+        return await self.dish_repository.update(
+            session, menu_id, submenu_id, dish_id, new_menu
         )
-        query = self.main_query.where(target_dish_id == Dishes.id)
-
-        await self.check_exists_id_and_title(
-            session,
-            target_menu_id,
-            target_submenu_id,
-            target_dish_id=target_dish_id,
-            title=new_dish.title,
-        )
-        await session.execute(stmt)
-        await session.commit()
-        result = await session.execute(query)
-        row = result.first()
-        dish = dict(zip(self.col, row), **{'price': str(row[-1])})
-        return ResponseDish(**dish)
 
     async def delete_dish(
-        self,
-        target_menu_id: UUID,
-        target_submenu_id: UUID,
-        target_dish_id: UUID,
-        session: AsyncSession,
+        self, session: AsyncSession, menu_id: UUID, submenu_id: UUID,
+        dish_id: UUID
     ) -> ResponseMessage:
-        """Delete dish from db and return message.
-
-        target_dish_id: Current dish id.
-        """
-        stmt = delete(Dishes).where(target_dish_id == Dishes.id)
-        await self.check_exists_id_and_title(
-            session, target_menu_id, target_submenu_id, target_dish_id=target_dish_id
+        return await self.dish_repository.delete(
+            session, menu_id, submenu_id, dish_id
         )
-        await session.execute(stmt)
-        await session.commit()
-
-        return ResponseMessage(status=True, message='The dish has been deleted')
-
-
-dish_service = DishService()
--- src/service/menu_service.py
+++ src/service/menu_service.py
@@ -1,149 +1,33 @@
 from uuid import UUID
 
-from fastapi import HTTPException
-from sqlalchemy import delete, distinct, func, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.models import Dishes, Menus, Submenus
-from src.schemas import BaseRequestModel, ResponseMenu, ResponseMessage
+from src.repository.menu_repository import MenuRepository
+from src.schemas import BaseRequestModel, ResponseMenu
 
 
 class MenuService:
-    main_query = (
-        select(
-            Menus.id,
-            Menus.title,
-            Menus.description,
-            func.count(distinct(Submenus.id)),
-            func.count(Dishes.title),
-        )
-        .outerjoin(Submenus, Menus.id == Submenus.menu_id)
-        .outerjoin(Dishes, Submenus.id == Dishes.submenu_id)
-        .group_by(Menus.id)
-    )
-
-    col = Menus.__table__.columns.keys()
-
-    @staticmethod
-    async def check_exists_id_and_title(
-        session: AsyncSession,
-        target_id: UUID | None = None,
-        title: str | None = None,
-    ) -> None:
-        query_check = select(Menus.id, Menus.title).where(target_id == Menus.id)
-        query_title = select(Menus.title).where(title == Menus.title)
-
-        if target_id:
-            result = await session.execute(query_check)
-            row = result.first()
-
-            if not row:
-                raise HTTPException(status_code=404, detail='menu not found')
-
-            if title:
-                if row[1] == title:
-                    raise HTTPException(
-                        status_code=409, detail='This title already exists'
-                    )
-
-        if title and not target_id:
-            result = await session.execute(query_title)
-            if result.first():
-                raise HTTPException(status_code=409, detail='This title already exists')
+    def __init__(self):
+        self.menu_repository = MenuRepository()
 
     async def get_all_menus(self, session: AsyncSession) -> list[ResponseMenu]:
-        """Get menus list from db and return them."""
-        result = await session.execute(self.main_query)
-        rows = result.fetchall()
-        menus = [
-            ResponseMenu(
-                **(
-                    dict(
-                        zip(self.col, row),
-                        **{'submenus_count': row[-2], 'dishes_count': row[-1]}
-                    )
-                )
-            )
-            for row in rows
-        ]
-        return menus
+        return await self.menu_repository.get_all(session)
 
     async def get_menu_by_id(
-        self, target_menu_id: UUID, session: AsyncSession
+        self, session: AsyncSession, menu_id: UUID
     ) -> ResponseMenu:
-        """Get menu from db and return it.
-
-        target_menu_id: Current menu id.
-        """
-        query = self.main_query.where(target_menu_id == Menus.id)
-        result = await session.execute(query)
-        row = result.first()
-
-        if not row:
-            raise HTTPException(status_code=404, detail='menu not found')
-
-        menu = dict(
-            zip(self.col, row), **{'submenus_count': row[-2], 'dishes_count': row[-1]}
-        )
-        return ResponseMenu(**menu)
+        return await self.menu_repository.get_by_id(session, menu_id)
 
     async def add_menu(
-        self, new_menu: BaseRequestModel, session: AsyncSession
+        self, session: AsyncSession, new_menu: BaseRequestModel
     ) -> ResponseMenu:
-        """Add new submenu to db and return it.
-
-        new_menu: Pydantic schema for request body.
-        """
-        stmt = insert(Menus).values(**dict(new_menu))
-        query = self.main_query.where(Menus.title == new_menu.title)
-
-        await self.check_exists_id_and_title(session, title=new_menu.title)
-        await session.execute(stmt)
-        await session.commit()
-        result = await session.execute(query)
-        row = result.fetchone()
-        menu = dict(
-            zip(self.col, row), **{'submenus_count': row[-2], 'dishes_count': row[-1]}
-        )
-        return ResponseMenu(**menu)
+        return await self.menu_repository.add(session, new_menu)
 
     async def update_menu(
-        self, target_menu_id: UUID, new_menu: BaseRequestModel, session: AsyncSession
+        self, session: AsyncSession, menu_id: UUID,
+        new_menu: BaseRequestModel
     ) -> ResponseMenu:
-        """Update menu in db and return it.
+        return await self.menu_repository.update(session, menu_id, new_menu)
 
-        target_menu_id: Current menu id.
-        new_menu: Pydantic schema for request body.
-        """
-        stmt = update(Menus).where(target_menu_id == Menus.id).values(**dict(new_menu))
-        query = self.main_query.where(target_menu_id == Menus.id)
-
-        await self.check_exists_id_and_title(
-            session, target_id=target_menu_id, title=new_menu.title
-        )
-        await session.execute(stmt)
-        await session.commit()
-        result = await session.execute(query)
-        row = result.first()
-        menu = dict(
-            zip(self.col, row), **{'submenus_count': row[-2], 'dishes_count': row[-1]}
-        )
-        return ResponseMenu(**menu)
-
-    async def delete_menu(
-        self, target_menu_id: UUID, session: AsyncSession
-    ) -> ResponseMessage:
-        """Delete menu from db and return message.
-
-        target_menu_id: Current menu id.
-        """
-        stmt = delete(Menus).where(target_menu_id == Menus.id)
-
-        await self.check_exists_id_and_title(session, target_id=target_menu_id)
-        await session.execute(stmt)
-        await session.commit()
-
-        return ResponseMessage(status=True, message='The menu has been deleted')
-
-
-menu_service = MenuService()
+    async def delete_menu(self, session: AsyncSession, menu_id: UUID):
+        return await self.menu_repository.delete(session, menu_id)
