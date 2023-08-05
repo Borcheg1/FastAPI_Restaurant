15b39ff9a5055b43bee1cede654aec12eb3e1f5a
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-08-05 18:15:59 +0300

    created mypy.ini

--- src/database.py
+++ src/database.py
@@ -1,11 +1,21 @@
 from typing import AsyncGenerator
 
+from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from src.config import (
+    DB_HOST,
+    DB_NAME,
+    DB_PASS,
+    DB_PORT,
+    DB_USER,
+    REDIS_HOST,
+    REDIS_PORT,
+)
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
 
 class Base(DeclarativeBase):
@@ -20,6 +30,11 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     """Get AsyncSession for connect to db."""
     async with async_session() as session:
         yield session
+
+
+async def get_redis_client() -> AsyncGenerator[Redis, None]:
+    async with redis as client:
+        yield client
 
 
 async def create_tables() -> None:
