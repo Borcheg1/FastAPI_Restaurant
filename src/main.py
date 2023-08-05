d84d6e8789f5f76dc68eef0e2675a6f90c5aff57
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-08-05 21:36:37 +0300

    reformatted openapi.json

--- src/main.py
+++ src/main.py
@@ -1,32 +1,21 @@
+import json
+
 from fastapi import FastAPI
 
 from src.database import create_tables
-from src.menus.router import router as menu_router
-from src.submenus.router import router as submenu_router
-from src.dishes.router import router as dish_router
+from src.router import main_router
 
-app = FastAPI(title="Restaurant API")
+app = FastAPI(title='Restaurant API')
 
-app.include_router(
-    menu_router,
-    prefix="/api/v1/menus",
-    tags=["Menu"]
-)
-app.include_router(
-    submenu_router,
-    prefix="/api/v1/menus/{target_menu_id}/submenus",
-    tags=["Submenu"]
-)
-app.include_router(
-    dish_router,
-    prefix="/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes",
-    tags=["Dish"]
-)
+app.include_router(main_router)
 
 
-@app.on_event("startup")
+def custom_openapi():
+    with open('openapi.json') as file:
+        return json.load(file)
+
+
+@app.on_event('startup')
 async def init_db() -> None:
-    """
-    Recreate tables in db after app launch
-    """
+    """Initial Redis and recreate tables in db after app launch."""
     await create_tables()
