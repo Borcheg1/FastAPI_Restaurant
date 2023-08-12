0356a9c715b39f124c03b5d102ae30fd072617b6
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-08-12 22:28:38 +0300

    updated src/router.py (added full_menu_router)

--- src/router.py
+++ src/router.py
@@ -1,18 +1,27 @@
 from fastapi import APIRouter
 
 from src.api.dish_router import router as dish_router
+from src.api.full_menu_router import router as full_menu_router
 from src.api.menu_router import router as menu_router
 from src.api.submenu_router import router as submenu_router
 
 main_router = APIRouter()
 
 main_router.include_router(menu_router, prefix='/api/v1/menus', tags=['Menu'])
+
 main_router.include_router(
     submenu_router, prefix='/api/v1/menus/{target_menu_id}/submenus',
     tags=['Submenu']
 )
+
 main_router.include_router(
     dish_router,
     prefix='/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes',
     tags=['Dish']
+)
+
+main_router.include_router(
+    full_menu_router,
+    prefix='/api/v1/all_data',
+    tags=['Get all data']
 )
